584323d7f036c82acd7e1d0453bd94ea8a471777
stettix <jan.stette@gmail.com>
2018-03-08 14:13:19 +0000

    Eval monad in Cats

---
+++ src/main/scala/net/janvsmachine/catsbook/ch4c.scala
@@ -0,0 +1,18 @@
+package net.janvsmachine.catsbook
+
+import cats.Eval
+
+object ch4c extends App {
+
+  // Playing around with the Eval monad.
+
+  def foldRight[A, B](as: List[A], acc: B)(fn: (A, B) => B): Eval[B] = as match {
+    case head :: tail =>
+      val recursiveResult: Eval[B] = Eval.defer(foldRight(tail, acc)(fn))
+      recursiveResult.map(res => fn(head, res))
+    case Nil =>
+      Eval.now(acc)
+  }
+
+  println(foldRight((1 to 50000).toList, 0)(_ + _).value)
+}
