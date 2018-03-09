f42c732ffd70f0194b81fe658cbd5a51cba404b7
stettix <jan.stette@gmail.com>
2018-03-09 12:24:11 +0000

    Writer monad exercise

---
+++ src/main/scala/net/janvsmachine/catsbook/ch4d.scala
@@ -0,0 +1,40 @@
+package net.janvsmachine.catsbook
+
+import cats.data.Writer
+import cats.instances.vector._
+import cats.syntax.applicative._
+import cats.syntax.writer._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
+
+object ch4d extends App {
+
+  def slowly[A](op: => A): A =
+    try op finally Thread.sleep(100)
+
+  type Logged[A] = Writer[Vector[String], A]
+
+  def factorial(n: Int): Logged[Int] = for {
+    ans <- if (n < 2) 1.pure[Logged] else slowly(factorial(n - 1).map(_ * n))
+    _ <- Vector(s"Fact $n = $ans").tell
+  } yield ans
+
+  val res: Logged[Int] = factorial(5)
+  val (logs, value) = res.run
+  println(s"V2: Logs = $logs, result = $value")
+
+  println("...")
+
+    val asyncRes: Seq[Logged[Int]] = Await.result(Future.sequence(Vector(
+      Future(factorial(5)), Future(factorial(5))
+    )), 10.seconds)
+
+    asyncRes.foreach { r =>
+      val (logs, value) = r.run
+      println(s"Value: $value, logs: ${logs.mkString(", ")}")
+    }
+
+}
